3a5bf719f6eef9cbc7bc011f3aaf9b051f51c25f
SKB-7 <152970115+SKB-7@users.noreply.github.com>
2024-05-08 21:38:42 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'xarray',
         'ecubevis',
         'tensorflow',
-        'scikit-learn' #'sklearn',
+        'scikit-learn', #'sklearn',
         'opencv-python',
         'joblib',
         'seaborn',
